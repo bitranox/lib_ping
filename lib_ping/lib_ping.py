4a47751ed00e372700242d2ba95ce541eac66eb9
bitranox <rnowotny@rotek.at>
2019-11-11 12:55:42 +0100

    refractor

--- lib_ping/lib_ping.py
+++ lib_ping/lib_ping.py
@@ -34,6 +34,9 @@ def ping(target: str, times: int = 4) -> ResponseObject:
     True
     >>> response.packets_lost_percentage
     0
+    >>> response = ping('10.0.0.1', 1)
+    >>> response.reached
+    False
 
     """
 
@@ -41,47 +44,51 @@ def ping(target: str, times: int = 4) -> ResponseObject:
     response.target = target
     response.number_of_pings = times
 
-    if lib_platform.is_platform_posix:
-        ping_result = ping_posix(target=target, times=times)
-    else:
-        ping_result = ping_windows(target=target)
+    try:
+        if lib_platform.is_platform_posix:
+            ping_result = ping_posix(target=target, times=times)
+        else:
+            ping_result = ping_windows(target=target)
 
-    text = ping_result.stdout.replace('\r\n', '\n').replace('\r', '\n')
+        text = ping_result.stdout.replace('\r\n', '\n').replace('\r', '\n')
 
-    # match IP address in format: [192.168.1.1] (192.168.1.1)
-    ip = re.findall(r'(?<=[(\[])\d+\.\d+\.\d+\.\d+(?=[)\]])', text)
-    if not ip:
-        ip = re.findall(r'\d+\.\d+\.\d+\.\d+', text)
-    response.ip = ip[0] if ip else '0.0.0.0'
+        # match IP address in format: [192.168.1.1] (192.168.1.1)
+        ip = re.findall(r'(?<=[(\[])\d+\.\d+\.\d+\.\d+(?=[)\]])', text)
+        if not ip:
+            ip = re.findall(r'\d+\.\d+\.\d+\.\d+', text)
+        response.ip = ip[0] if ip else '0.0.0.0'
 
-    # avg ping time
-    if lib_platform.is_platform_posix:
-        time = re.findall(r'(?=\d+\.\d+/)(\d+\.\d+)+', text)
-        if time:                                                                                                     # pragma: no cover
-            response.time_min_ms = float(time[0])
-            response.time_avg_ms = float(time[1])
-            response.time_max_ms = float(time[2])
-    else:
-        time = re.findall(r'(\d+(?=ms))+', text)
-        if time:                                                                                                     # pragma: no cover
-            response.time_avg_ms = float(time[len(time) - 1])
-            response.time_max_ms = float(time[len(time) - 2])
-            response.time_min_ms = float(time[len(time) - 3])
+        # avg ping time
+        if lib_platform.is_platform_posix:
+            time = re.findall(r'(?=\d+\.\d+/)(\d+\.\d+)+', text)
+            if time:                                                                                                     # pragma: no cover
+                response.time_min_ms = float(time[0])
+                response.time_avg_ms = float(time[1])
+                response.time_max_ms = float(time[2])
+        else:
+            time = re.findall(r'(\d+(?=ms))+', text)
+            if time:                                                                                                     # pragma: no cover
+                response.time_avg_ms = float(time[len(time) - 1])
+                response.time_max_ms = float(time[len(time) - 2])
+                response.time_min_ms = float(time[len(time) - 3])
 
-    if not time:                                                                                                     # pragma: no cover
-        response.time_min_ms = response.time_avg_ms = response.time_max_ms = -1                                      # pragma: no cover
+        if not time:                                                                                                     # pragma: no cover
+            response.time_min_ms = response.time_avg_ms = response.time_max_ms = -1                                      # pragma: no cover
 
-    # packet loss rate
-    lost = re.findall(r'\d+.\d+(?=%)', text)
-    if not lost:
-        lost = re.findall(r'\d+(?=%)', text)
+        # packet loss rate
+        lost = re.findall(r'\d+.\d+(?=%)', text)
+        if not lost:
+            lost = re.findall(r'\d+(?=%)', text)
 
-    response.n_packets_lost = len(lost)
-    response.packets_lost_percentage = int(round(float(lost[len(lost) - 1]))) if lost else 100
-    if response.packets_lost_percentage < 100:                                                                       # pragma: no cover
-        response.reached = True
-    _create_str_result(response=response)
-    return response
+        response.n_packets_lost = len(lost)
+        response.packets_lost_percentage = int(round(float(lost[len(lost) - 1]))) if lost else 100
+        if response.packets_lost_percentage < 100:                                                                       # pragma: no cover
+            response.reached = True
+        _create_str_result(response=response)
+    except subprocess.CalledProcessError:
+        pass
+    finally:
+        return response
 
 
 def _create_str_result(response: ResponseObject) -> str:
@@ -106,13 +113,13 @@ def ping_windows(target: str) -> lib_shell.ShellCommandResponse:
 
 def ping_windows_ipv4(target: str) -> lib_shell.ShellCommandResponse:
     cmd = 'ping -w 2000 ' + target
-    response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet)
+    response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet, retries=1)
     return response
 
 
 def ping_windows_ipv6(target: str) -> lib_shell.ShellCommandResponse:
     cmd = 'ping -w 2000 ' + target
-    response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet)
+    response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet, retries=1)
     return response
 
 
@@ -120,6 +127,9 @@ def ping_posix(target: str, times: int) -> lib_shell.ShellCommandResponse:
     """
     >>> if lib_platform.is_platform_posix:
     ...     response = ping_posix(target='1.1.1.1', times=1)
+    >>> if lib_platform.is_platform_posix:
+    ...     response = ping_posix(target='10.0.0.1', times=1)
+
     """
     try:
         response = ping_posix_ipv4(target=target, times=times)
@@ -129,22 +139,29 @@ def ping_posix(target: str, times: int) -> lib_shell.ShellCommandResponse:
 
 
 def ping_posix_ipv4(target: str, times: int) -> lib_shell.ShellCommandResponse:
+    """
+    >>> if lib_platform.is_platform_posix:
+    ...     response = ping_posix_ipv4(target='1.1.1.1', times=1)
+    >>> if lib_platform.is_platform_posix:
+    ...     response = ping_posix_ipv4(target='10.0.0.1', times=1)
+
+    """
     # ping -i parameter decimal sign can be different (0.2 or 0,2) on different linux versions
     try:
-        cmd = 'ping -c {times} -W2000 -i 0.2 {target}'.format(times=times, target=target)
-        response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet)
+        cmd = 'ping -c {times} -W2000 -i 0.2 {target} -w 1'.format(times=times, target=target)
+        response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet, retries=1)
     except subprocess.CalledProcessError:
-        cmd = 'ping -c {times} -W2000 -i 0,2 {target}'.format(times=times, target=target)
-        response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet)
+        cmd = 'ping -c {times} -W2000 -i 0,2 {target} -w 1'.format(times=times, target=target)
+        response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet, retries=1)
     return response
 
 
 def ping_posix_ipv6(target: str, times: int) -> lib_shell.ShellCommandResponse:
     # ping -i parameter decimal sign can be different (0.2 or 0,2) on different linux versions
     try:
-        cmd = 'ping -6 -c {times} -W2000 -i 0.2 {target}'.format(times=times, target=target)
-        response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet)
+        cmd = 'ping -6 -c {times} -W2000 -i 0.2 {target} -w 1'.format(times=times, target=target)
+        response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet, retries=1)
     except subprocess.CalledProcessError:
-        cmd = 'ping -6 -c {times} -W2000 -i 0,2 {target}'.format(times=times, target=target)
-        response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet)
+        cmd = 'ping -6 -c {times} -W2000 -i 0,2 {target} -w 1'.format(times=times, target=target)
+        response = lib_shell.run_shell_command(command=cmd, shell=True, log_settings=lib_shell.conf_lib_shell.log_settings_qquiet, retries=1)
     return response
